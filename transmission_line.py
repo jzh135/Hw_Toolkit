66c00bddcb3171f3eb5f93458514217a0afc2160
Jin Zhang <yexingzhentan88@gmail.com>
2025-06-25 12:50:30 -0400

    add cal_coaxial_params

--- transmission_line.py
+++ transmission_line.py
@@ -4,8 +4,8 @@ import numpy as np
 def cal_two_wire_params (d: float,          # Radius of each conductor
                          D: float,          # Center-to-center distance between the two conductors
                          epsilon_r: float,  # Relative Permittivity (dielectric constant) = 1 in air
-                         mu: float,         # Magnetic permeability of the medium (mu_0 for air)
-                         mu_r: float,       # Relative Magnetic Permeability of the conductor
+                         mu_ri: float,      # Relative Magnetic permeability of the insulator (1 for air)
+                         mu_rc: float,      # Relative Magnetic Permeability of the conductor
                          sigma_c: float,    # Electrical Conductivity of conductor
                          length: float,     # Length of the cable
                          freq: float) -> dict:
@@ -14,7 +14,8 @@ def cal_two_wire_params (d: float,          # Radius of each conductor
     epsilon_0 = 8.854e-12 # Vacuum permittivity (F/m)
     epsilon = epsilon_0 * epsilon_r
     mu_0 = 4 * np.pi * 1e-7 # Vacuum permeability (H/m)
-    mu_c = mu_0 * mu_r
+    mu_c = mu_0 * mu_rc
+    mu_i = mu_0 * mu_ri
 
     # Calculate surface resistance (due to skin effect)
     Rs = np.sqrt(np.pi * freq * mu_c / sigma_c)
@@ -22,7 +23,7 @@ def cal_two_wire_params (d: float,          # Radius of each conductor
     R_per_m = 2 * Rs / (np.pi * d)
 
     # Calculate the unit inductance
-    L_per_m = mu/np.pi * np.log(D/d + np.sqrt((D/d)**2 - 1))
+    L_per_m = mu_i/np.pi * np.log(D/d + np.sqrt((D/d)**2 - 1))
 
     # Calculate the unit capacitance
     C_per_m = np.pi * epsilon / np.log((D/d) + np.sqrt((D/d)**2 - 1))
@@ -37,6 +38,45 @@ def cal_two_wire_params (d: float,          # Radius of each conductor
         "C": C
     }
 
+def cal_coaxial_params (a: float,           # Inner conductor radius (meters)
+                        b: float,           # Outer conductor radius (meters)
+                        epsilon_r: float,   # Relative Permittivity (dielectric constant) = 1 in air
+                        mu_ri: float,       # Relative Magnetic permeability of the insulator (1 for air)
+                        mu_rc: float,       # Relative Magnetic Permeability of the conductor
+                        sigma_c: float,     # Electrical Conductivity of conductor
+                        length: float,      # Length of the cable
+                        freq: float) -> dict:
+    
+    # Constants
+    epsilon_0 = 8.854e-12 # Vacuum permittivity (F/m)
+    epsilon = epsilon_0 * epsilon_r
+    mu_0 = 4 * np.pi * 1e-7 # Vacuum permeability (H/m)
+    mu_c = mu_0 * mu_rc
+    mu_i = mu_0 * mu_ri
+
+    # Calculate surface resistance (due to skin effect)
+    Rs = np.sqrt(np.pi * freq * mu_c / sigma_c)
+    # Calculate the resistance per unit length
+    R_per_m = Rs/(2*np.pi) * (1/a + 1/b)
+
+    # Calculate the resistance per unit length
+    L_per_m = mu_i/(2*np.pi) * np.log(b/a)
+
+    # Calculate the capacitance per unit length
+    C_per_m = 2 * np.pi * epsilon / np.log(b/a)
+    
+    R = R_per_m * length
+    L = L_per_m * length
+    C = C_per_m * length
+
+    return {
+        "R": R,
+        "L": L,
+        "C": C
+    }
+
+
+
 if __name__ == "__main__":
     # Define parameters
     
@@ -44,15 +84,15 @@ if __name__ == "__main__":
     d = 0.000813            # Radius of each conductor (14 AWG)
     D = 0.02                # Center-to-center distance between the two conductors
     epsilon_r = 1           # Relative Permittivity (air)
-    mu = 4 * np.pi * 1e-7   # Magnetic permeability of the medium (mu_0 for air)
-    mu_r = 1                # Relative Magnetic Permeability of the conductor (copper)
+    mu_ri = 1               # Relative Magnetic permeability of the medium (1 for air)
+    mu_rc = 1                # Relative Magnetic Permeability of the conductor (copper)
     sigma_c = 5.8e7         # Electrical Conductivity of conductor (copper)
     length = 2500           # Length of the cable (meters)
     freq = 60               # Frequency (Hz)
 
 
     
-    params = cal_two_wire_params(d, D, epsilon_r, mu, mu_r, sigma_c, length, freq)
+    params = cal_two_wire_params(d, D, epsilon_r, mu_ri, mu_rc, sigma_c, length, freq)
 
     # Print out results
     for key, value in params.items():
